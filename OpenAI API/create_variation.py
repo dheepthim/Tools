ea5f5e54e74ab968bf2e73fd4e8b05ad9bcaa91b
Dheepthi Mohanraj <52431063+dheepthim@users.noreply.github.com>
2023-11-26 11:31:43 -0500

    Variations!

---
+++ OpenAI API/create_variation.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+import requests
+
+client = OpenAI()
+
+response = client.images.create_variation(
+  image = open("OpenAI API/PNGs/image1.png", "rb"),
+  model = "dall-e-2",
+  n = 1,
+  response_format = "url",
+  size = "1024x1024"
+)
+
+data = requests.get(response.data[0].url).content
+with open('OpenAI API/Variations/variations1-1.png', 'wb') as fh:
+  fh.write(data)
